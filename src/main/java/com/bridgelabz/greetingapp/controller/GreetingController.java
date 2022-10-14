69e47825acfe7bf974dcb8f2898b466a2d2af562
kasturi28 <kasturipansare2818@gmail.com>
2022-10-14 10:47:41 +0530

    Added:Service layer message

---
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.greetingapp.controller;
+
+import com.bridgelabz.greetingapp.entity.Greeting;
+import com.bridgelabz.greetingapp.service.IGreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private static final String template = "Hello, %s";
+    private IGreetingService greetingService;
+
+    private final AtomicLong counter= new AtomicLong();
+
+    public GreetingController(IGreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/")
+    public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return greetingService.getGreetingMessage();
+    }
+
+}
