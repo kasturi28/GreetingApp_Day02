2971eb8bf108051639b0363b318b59927f3197ac
kasturi28 <kasturipansare2818@gmail.com>
2022-10-16 21:29:08 +0530

    Added:Get greeting message list using GetMapping

--- src/main/java/com/bridgelabz/greetingapp01/service/IGreetingService.java
+++ src/main/java/com/bridgelabz/greetingapp01/service/IGreetingService.java
@@ -3,10 +3,14 @@ package com.bridgelabz.greetingapp01.service;
 import com.bridgelabz.greetingapp01.entity.Greeting;
 import com.bridgelabz.greetingapp01.entity.User;
 
+import java.util.List;
+
 public interface IGreetingService {
     String getGreetingMessage();
     String getGreetingMessage(User user);
     Greeting addGreetingMessage(User user);
 
     Greeting getGreetingMsgById(long id);
+
+    List<Greeting> getAllGreetingMsg();
 }
